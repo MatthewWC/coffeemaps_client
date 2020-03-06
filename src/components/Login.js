2b3ea9e573a698195957a810b35659986d435663
Matthew Cook <matthew.w.cook94@gmail.com>
2020-03-06 13:14:10 -0500

    fixed login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,146 @@
 import React from 'react'
 import gql from 'graphql-tag'
 
+import validator from '../tools/validator'
+
+// ---- M-UI imports ----
+import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
+import { Typography } from '@material-ui/core'
+import FormHelperText from '@material-ui/core/FormHelperText'
+// ---- M-UI imports ----
 
-import FormBuilder from '../tools/FormBuilder'
+// M-UI styles
+const useStyles = makeStyles(theme => ({
+  registerRoot: {
+    flexDirection: 'column'
+  },
+  paper: {
+    minWidth: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 10,
+    padding: 20,
+  },
+  title: {
+    marginBottom: 20,
+    margin: 'auto'
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '250px' // set height for proper spacing
+  },
+  textField: { 
+  },
+  errors: {
+    margin: theme.spacing(2)
+  },  
+  button: {
+  }
+}))
 
-//TODO: add register link
+function Login(props) {
 
-function Login (props) {
+  // M-UI styles instance
+  const classes = useStyles()
+
+  //refs
+  const errorRef = React.createRef()
+  const formRef = React.createRef()
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    // reset error text
+    errorRef.current.innerHTML = ''
+    let variables = {}
+
+    try{
+      validator(formRef.current)
+      // INSERT MUTATION LOGIC
+      Object.keys(formRef.current).map(field => {
+        switch(formRef.current[field].type){
+          case 'text':
+            return variables[formRef.current[field].name] = formRef.current[field].value
+          case 'password':
+            return variables[formRef.current[field].name] = formRef.current[field].value
+          default:
+            return null
+        }
+      })
+      const results = await props.client.mutate({
+        mutation: LOGIN,
+        variables: variables
+      })
+      localStorage.setItem('token', results.data.login.token)
+      localStorage.setItem('email', results.data.login.user.email)
+      localStorage.setItem('role', results.data.login.user.role)
+      props.history.push('/')
+    }
+    catch(error){
+      let errMsg
+      // handle unsuccessful response
+      errMsg = error.toString().lastIndexOf(':') + 1
+      errorRef.current.innerHTML = error.toString().substring(errMsg, 75)
+    }
+  }
 
   return(
-    <Grid container justify='center'>
-      <FormBuilder
-        formName={'Login Form'}
-        fields={{
-          email: 'String',
-          password: 'String'
-        }}
-        mutation={LOGIN}
-        props={props}/>
+    <Grid
+      container
+      justify='center'>
+      <Paper
+        className={classes.paper}>
+        <Typography
+          className={classes.title}>
+          Login
+        </Typography>
+        <form
+          name='Login Form'
+          className={classes.form}
+          ref={formRef}>
+          <TextField
+            key='email'
+            className={classes.textField}
+            name='email'
+            label='Email'
+            variant='outlined'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            type='text'
+            /> 
+          <TextField
+            key='password'
+            className={classes.textField}
+            name='password'
+            label='Password'
+            variant='outlined'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            type='password'
+            /> 
+          <FormHelperText
+            className={classes.errors}
+            ref={errorRef}
+            error={true}
+          >
+          </FormHelperText>
+          <Button
+            onClick={(event) => {
+              handleSubmit(event)
+            }}
+            className={classes.button}
+            variant='contained'>
+              Login
+          </Button>
+        </form>
+      </Paper>
     </Grid>
   )
 }
