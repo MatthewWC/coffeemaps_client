8c3f1b88eb31c13435582371ad485187f8db2da6
Matthew Cook <matthew.w.cook94@gmail.com>
2020-03-06 13:15:02 -0500

    changed centering

--- src/components/MapView/Map.js
+++ src/components/MapView/Map.js
@@ -13,14 +13,14 @@ const useStyles = makeStyles(theme => ({
     position: 'absolute',
     right: 0,
     left: 0,
-    top: 0,
+    top: 50,
     bottom: 0,
     overflow: 'hidden'
   }
 }))
 
 function Map ({ client, onStoreMarkerClicked }) {
-
+  
   // M-UI styles instance
   const classes = useStyles()
   
@@ -29,37 +29,36 @@ function Map ({ client, onStoreMarkerClicked }) {
   
   useEffect(() => {
     async function initMap(){
-      
-      mapRef.current = L.map('map', {
-        // map starting spot
-        center: [10, 10],
-        zoom: 12,
-        zoomControl: false,
-        layers: [
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            detectRetina: true,
-            maxZoom: 19,
-          })
-        ]
-      }, []) 
-    }
-    initMap()
-    .then(async () => {
-      const stores = await client.query({query: GET_STORES})
-      stores.data.getStores.map(store => {
-        return L.marker([store.latitude, store.longitude])
-            .addTo(mapRef.current)
-            .bindTooltip(store.storeName, { className: 'myCSSClass', permanent: true})
-            .on('click', () => {
-              mapRef.current.flyTo([store.latitude, store.longitude])
-              onStoreMarkerClicked(store)
+        mapRef.current = L.map('map', {
+          // map starting spot
+          center: [39.8283, -98.5795],
+          zoom: 5,
+          zoomControl: false,
+          layers: [
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              detectRetina: true,
+              maxZoom: 19,
             })
-      })
-    })
+          ]
+        }, []) 
+      }
+      initMap()
+      .then(async () => {
+        const stores = await client.query({query: GET_STORES})
+        stores.data.getStores.map(store => {
+          return L.marker([store.latitude, store.longitude])
+              .addTo(mapRef.current)
+              .bindTooltip(store.storeName, { className: 'myCSSClass', permanent: true})
+              .on('click', () => {
+                mapRef.current.flyTo([store.latitude, store.longitude])
+                onStoreMarkerClicked(store)
+              })
+        })
+      })     
     // annoying warning disabled, cant figure out any other workaround
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-
+  
   return (
     <div className={classes.map} id='map'/>
   )
